0d6bd21520a8b3995d4450dfe4e5e7d937b8f4e9
A0268 <119771520+A0268@users.noreply.github.com>
2025-10-15 23:22:17 +0800

    Create semi.py

---
+++ dataset/semi.py
@@ -0,0 +1,199 @@
+from dataset.transform import *
+import math
+import os
+from PIL import Image
+import random
+from torch.utils.data import Dataset
+from torchvision import transforms
+import numpy as np
+import torch
+from copy import deepcopy
+
+
+class SemiDataset0(Dataset):
+    def __init__(self, name, root, mode, size, labeled_id_path=None, unlabeled_id_path=None, pseudo_mask_path=None):
+        self.name = name
+        self.root = root
+        self.mode = mode
+        self.size = size
+
+        self.pseudo_mask_path = pseudo_mask_path
+
+        if mode == 'semi_train':
+            with open(labeled_id_path, 'r') as f:
+                self.labeled_ids = f.read().splitlines()
+            with open(unlabeled_id_path, 'r') as f:
+                self.unlabeled_ids = f.read().splitlines()
+            self.ids = \
+                self.labeled_ids * math.ceil(len(self.unlabeled_ids) / len(self.labeled_ids)) + self.unlabeled_ids
+
+        else:
+            if mode == 'val':
+                id_path = 'dataset/splits/%s/val.txt' % name
+            elif mode == 'label' or mode == 'select_unlabeled':
+                id_path = unlabeled_id_path
+            elif mode == 'train' or mode == 'select_labeled':
+                id_path = labeled_id_path
+
+            with open(id_path, 'r') as f:
+                self.ids = f.read().splitlines()
+
+    def __getitem__(self, item):
+        id = self.ids[item]
+        img = Image.open(os.path.join(self.root, id.split(' ')[0]))
+
+        if self.mode == 'val' or self.mode == 'label':
+            mask = Image.open(os.path.join(self.root, id.split(' ')[1]))
+            img, mask = normalize(img, mask)
+            return img, mask, id
+        
+
+        if self.mode == 'train' or (self.mode == 'semi_train' and id in self.labeled_ids):
+            mask = Image.open(os.path.join(self.root, id.split(' ')[1]))
+        else:
+            # mode == 'semi_train' and the id corresponds to unlabeled image
+            fname = os.path.basename(id.split(' ')[1])
+            mask = Image.open(os.path.join(self.pseudo_mask_path, fname))
+
+        img, mask = resize(img, mask, (0.5, 2.0))
+        img, mask = crop(img, mask, self.size)
+        img, mask = hflip(img, mask, p=0.5)
+        img, mask = normalize(img, mask)
+
+        return img, mask
+
+    def __len__(self):
+        return len(self.ids)
+
+
+class SemiDataset1(Dataset):
+    def __init__(self, name, root, mode, size, labeled_id_path=None, unlabeled_id_path=None, pseudo_mask_path=None):
+
+        self.name = name
+        self.root = root
+        self.mode = mode
+        self.size = size
+
+        self.pseudo_mask_path = pseudo_mask_path
+
+        if mode == 'semi_train':
+            with open(labeled_id_path, 'r') as f:
+                self.labeled_ids = f.read().splitlines()
+            with open(unlabeled_id_path, 'r') as f:
+                self.unlabeled_ids = f.read().splitlines()
+            self.ids = \
+                self.labeled_ids * math.ceil(len(self.unlabeled_ids) / len(self.labeled_ids)) + self.unlabeled_ids
+
+        else:
+            if mode == 'val':
+                id_path = 'dataset/splits/%s/val.txt' % name
+            elif mode == 'label' or mode == 'select_unlabeled':
+                id_path = unlabeled_id_path
+            elif mode == 'train' or mode == 'select_labeled':
+                id_path = labeled_id_path
+
+            with open(id_path, 'r') as f:
+                self.ids = f.read().splitlines()
+
+    def __getitem__(self, item):
+        id = self.ids[item]
+        img = Image.open(os.path.join(self.root, id.split(' ')[0]))
+
+        if self.mode == 'val' or self.mode == 'label' or self.mode == 'select_labeled':
+            mask = Image.open(os.path.join(self.root, id.split(' ')[1]))
+            img, mask = normalize(img, mask)
+            return img, mask, id
+        
+        if self.mode == 'select_unlabeled':
+            fname = os.path.basename(id.split(' ')[1])
+            mask = Image.open(os.path.join(self.pseudo_mask_path, fname))
+            img, mask = normalize(img, mask)
+            return img, mask, id
+
+
+        if self.mode == 'train' or (self.mode == 'semi_train' and id in self.labeled_ids):
+            mask = Image.open(os.path.join(self.root, id.split(' ')[1]))
+        else:
+            # mode == 'semi_train' and the id corresponds to unlabeled image
+            fname = os.path.basename(id.split(' ')[1])
+            mask = Image.open(os.path.join(self.pseudo_mask_path, fname))
+
+        img, mask = resize(img, mask, (0.5, 2.0))
+        img, mask = crop(img, mask, self.size)
+        img, mask = hflip(img, mask, p=0.5)
+
+        # strong augmentation on unlabeled images
+        if self.mode == 'semi_train' and id in self.unlabeled_ids:
+            if random.random() < 0.8:
+                img = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img)
+            img = transforms.RandomGrayscale(p=0.2)(img)
+            img = blur(img, p=0.5)
+            img, mask = cutout(img, mask, p=0.5)
+
+        img, mask = normalize(img, mask)
+
+        return img, mask, id
+
+    def __len__(self):
+        return len(self.ids)
+
+
+class SemiDataset2(Dataset):
+    def __init__(self, name, root, mode, size=None, id_path=None, nsample=None):
+        self.name = name
+        self.root = root
+        self.mode = mode
+        self.size = size
+
+        if mode == 'train_l' or mode == 'train_u':
+            with open(id_path, 'r') as f:
+                self.ids = f.read().splitlines()
+            if mode == 'train_l' and nsample is not None:
+                self.ids *= math.ceil(nsample / len(self.ids))
+                self.ids = self.ids[:nsample]
+        else:
+            with open('splits/%s/val.txt' % name, 'r') as f:
+                self.ids = f.read().splitlines()
+
+    def __getitem__(self, item):
+        id = self.ids[item]
+        img = Image.open(os.path.join(self.root, id.split(' ')[0])).convert('RGB')
+        mask = Image.fromarray(np.array(Image.open(os.path.join(self.root, id.split(' ')[1]))))
+
+        if self.mode == 'val':
+            img, mask = normalize(img, mask)
+            return img, mask, id
+
+        img, mask = resize(img, mask, (0.5, 2.0))
+        img, mask = crop(img, mask, self.size) 
+        img, mask = hflip(img, mask, p=0.5)
+
+        if self.mode == 'train_l':
+            return normalize(img, mask)
+
+        img_w, img_s1, img_s2 = deepcopy(img), deepcopy(img), deepcopy(img)
+
+        if random.random() < 0.8:
+            img_s1 = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img_s1)
+        img_s1 = transforms.RandomGrayscale(p=0.2)(img_s1)
+        img_s1 = blur(img_s1, p=0.5)
+        cutmix_box1 = obtain_cutmix_box(img_s1.size[0], p=0.5)
+
+        if random.random() < 0.8:
+            img_s2 = transforms.ColorJitter(0.5, 0.5, 0.5, 0.25)(img_s2)
+        img_s2 = transforms.RandomGrayscale(p=0.2)(img_s2)
+        img_s2 = blur(img_s2, p=0.5)
+        cutmix_box2 = obtain_cutmix_box(img_s2.size[0], p=0.5)
+
+        ignore_mask = Image.fromarray(np.zeros((mask.size[1], mask.size[0])))
+
+        img_s1, ignore_mask = normalize(img_s1, ignore_mask)
+        img_s2 = normalize(img_s2)
+
+        mask = torch.from_numpy(np.array(mask)).long()
+        ignore_mask[mask == 254] = 255
+
+        return normalize(img_w), img_s1, img_s2, ignore_mask, cutmix_box1, cutmix_box2
+
+    def __len__(self):
+        return len(self.ids)
